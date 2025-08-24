7133830a33f82ee23d1027d333bf5482f0e757e6
sznckz90-cloud <sznckz90@gmail.com>
2025-08-24 20:39:38 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
       if (capturedJsonResponse) {
         logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
       }
-
+      
       if (logLine.length > 80) {
         logLine = logLine.slice(0, 79) + "…";
       }
