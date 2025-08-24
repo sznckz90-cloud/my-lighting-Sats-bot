58ca728c914cf41e13f8dde175057d925ae74c46
sznckz90-cloud <sznckz90@gmail.com>
2025-08-24 19:34:34 +0530

    Update main.tsx

--- client/scr/main.tsx
+++ client/scr/main.tsx
@@ -1,5 +1,16 @@
+// client/src/main.tsx
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Could not find element with id 'root'");
+}
+
+createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
