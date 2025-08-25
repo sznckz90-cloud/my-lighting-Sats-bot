5a3531884ab3b8d59f28ba53dd2ea70bb8059118
sznckz90-cloud <sznckz90@gmail.com>
2025-08-26 00:55:08 +0530

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,4 +1,4 @@
-type { Express } from "express";
+import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
 import { insertUserSchema, insertWithdrawalRequestSchema } from "@shared/schema";
