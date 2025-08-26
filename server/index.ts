0fb123aaf8d0e725818ad7bc991bd096359c51df
sznckz90-cloud <sznckz90@gmail.com>
2025-08-26 17:59:05 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -61,6 +61,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10) || 5000;
+
   console.log(`Starting server on port: ${port} (PORT env: ${process.env.PORT})`);
   
   server.listen(port, "0.0.0.0", () => {
